fca2ddc35f9aa6bb482bca294d0af7034906b556
Zingam <zingam@outlook.com>
2018-10-08 18:52:46 +0300

    Game: Added product configuration values

--- GunBox/Sources/Game/Game_SourceFiles.cmake
+++ GunBox/Sources/Game/Game_SourceFiles.cmake
@@ -1,6 +1,7 @@
 include_guard ()
 
 set (Game_SourceFiles
+  "Sources/Configuration/Values.hpp"
   "Sources/Game.cpp"
   "Sources/Game.hpp"
   "Sources/GameFactory.cpp"
---
+++ GunBox/Sources/Game/Sources/Configuration/Values.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+////////////////////////////////////////////////////////////////////////////////
+// Product strings
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(Game_Developer)
+#  define Game_Developer "RMM"
+#else
+#  error Game_Developer is already defined...
+#endif
+
+#if !defined(Game_Name)
+#  define Game_Name "GunBox"
+#else
+#  error Game_Name is already defined...
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// Version numbers
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(Game_VersionBuildNumber)
+#  define Game_VersionBuildNumber 0
+#else
+#  error Game_VersionBuildNumber is already defined...
+#endif
+
+#if !defined(Game_VersionMajor)
+#  define Game_VersionMajor 0
+#else
+#  error Game_VersionMajor is already defined...
+#endif
+
+#if !defined(Game_VersionMinor)
+#  define Game_VersionMinor 1
+#else
+#  error Game_VersionMinor is already defined...
+#endif
+
+#if !defined(Game_VersionPatch)
+#  define Game_VersionPatch 0
+#else
+#  error Game_VersionPatch is already defined...
+#endif
