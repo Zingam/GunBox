a9d49fb453fe977f4517f39fbb37a4d780f191b8
Zingam <zingam@outlook.com>
2019-05-25 13:44:38 +0300

    Engine: [Vulkan] Removed an unnecessary Vulkan validation layer

--- GunBox/Sources/Engine/Sources/System/Platforms/Common/HostPlatform/GPUDevice_Vulkan_Base_Common.cpp
+++ GunBox/Sources/Engine/Sources/System/Platforms/Common/HostPlatform/GPUDevice_Vulkan_Base_Common.cpp
@@ -8,10 +8,7 @@ NAMESPACE_BEGIN(System::HostPlatformClasses)
 ////////////////////////////////////////////////////////////////////////////////
 
 GPUDevice_Vulkan_Base_Common::GPUDevice_Vulkan_Base_Common()
-  : validationLayerNames{
-    "VK_LAYER_KHRONOS_validation",
-    "VK_LAYER_RENDERDOC_Capture",
-  }
+  : validationLayerNames{ "VK_LAYER_KHRONOS_validation" }
 {}
 
 NAMESPACE_END(System::HostPlatformClasses)
